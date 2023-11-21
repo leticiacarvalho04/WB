500b5b5546adfc4c7db72d5106d82483bec97191
Letícia <110743347+leticiacarvalho04@users.noreply.github.com>
2023-11-21 09:40:49 -0300

    inserts mysql dos dados da tabela prisma

---
+++ back/db/tabela cpfsql.sql
@@ -0,0 +1,37 @@
+/*
+-- Query: select concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');') from cpf
+LIMIT 0, 1000
+
+-- Date: 2023-11-21 09:14
+*/
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'11112222333\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'11223344556\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'12341234123\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'12344321555\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'12345678901\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'12435465768\', \'2023-11-20 23:07:31.696\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'1295836284\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'13557907423\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'22223333444\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'23242424232\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'33332222111\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'38213948603\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'43214321432\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'44443333222\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'44445555666\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'4859282135\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'55554444333\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'55555555555\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'66665555777\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'66667777888\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'77776666555\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'77778888999\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'78907890789\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'88887777666\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'89436126490\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'93471364134\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'93723613130\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'98765432100\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'98765432101\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'99889988998\', \'1970-01-01 00:00:00.000\');');
+INSERT INTO `` (`concat('insert into cpf(valor, dataEmissao) values(''',valor,''', ''',dataEmissao,''');')`) VALUES ('insert into cpf(valor, dataEmissao) values(\'99998888777\', \'1970-01-01 00:00:00.000\');');
